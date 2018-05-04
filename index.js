0844a6db07e6e2b30fbb733017d852d91de4a12c
James Ferguson <ncjamesferguson@gmail.com>
2018-05-04 19:44:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,16 @@ function distanceTravelledInFeet(start, end) {
 }
 
 function calculatesFarePrice(start, destination) {
+  let distance = distanceTravelledInFeet(start, end)
 
+  switch (true) {
+    case distance < 400:
+    return 0;
+    case distance >= 400 && distance < 2000:
+    return 0.02 * (distance - 400);
+    case distance >= 2000 && distance <= 2500:
+    return 25.0;
+    case distance > 2500
+    return 'cannot travel that far';
+  }
 }
